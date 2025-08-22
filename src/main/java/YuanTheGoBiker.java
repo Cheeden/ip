ba52e66b72bff1916987433c98c57888e94951cf
Cheeden <jaydenlim27@gmail.com>
2025-08-22 12:54:43 +0800

    implemented user input with chatbot echoing and exits with command bye

--- src/main/java/YuanTheGoBiker.java
+++ src/main/java/YuanTheGoBiker.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class YuanTheGoBiker {
+    static String line = "____________________________________________";
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(line);
         System.out.println("Hello! I'm Yuan");
-        System.out.println("How can I help you? Do you need a Gojek?");
-        System.out.println("____________________________________________");
-        System.out.println("Bye. Hope to see you again");
-        System.out.println("____________________________________________");
+        System.out.println("How can I help you?");
+        System.out.println(line);
+
+        String input = "";
+
+        while (!input.equals("bye")) {
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("    " + line);
+                System.out.println("    " + "Bye. Hope to see you again");
+                System.out.println("    " + line);
+                break;
+            }
+
+            System.out.println("    " + line);
+            System.out.println("    " + input);
+            System.out.println("    " + line);
+        }
     }
 }
