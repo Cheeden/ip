e0b437491b0505c97ea3036364c6f5ae3e17cfd5
Cheeden <jaydenlim27@gmail.com>
2025-08-22 12:27:30 +0800

    Rename chatbot, added inital greeting and exit messages

---
+++ src/main/java/YuanTheGoBiker.java
@@ -0,0 +1,10 @@
+public class YuanTheGoBiker {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________");
+        System.out.println("Hello! I'm Yuan");
+        System.out.println("How can I help you? Do you need a Gojek?");
+        System.out.println("____________________________________________");
+        System.out.println("Bye. Hope to see you again");
+        System.out.println("____________________________________________");
+    }
+}
